1f7dacec9a11ee72222b6e805f47208b6ae6104c
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-08-08 18:55:49 -0500

    Added new files

---
+++ TAREA 1 - REPASO POO/board.h
@@ -0,0 +1,32 @@
+/*
+    Made by: 
+        - Diego Efraín Antonio Pérez 
+        - Armando Arredondo Valle
+*/
+
+
+using namespace std;
+#include <string>
+#include <vector>
+
+class Board{
+    private:
+        int num_snakes;
+        int num_ladders;
+        int num_cells;
+        int num_players;
+        int num_dice;
+        int num_turns;
+        int prison_cell;
+        int start_cell;
+        int end_cell;
+        int dice_roll;
+        vector<int> ladder_cells;
+        vector<int> snake_cells;
+        vector<int> snake_length;
+        vector<int> snake_direction;
+        vector<int> ladder_length;
+        vector<int> ladder_direction;
+    public:
+        
+};
---
+++ TAREA 1 - REPASO POO/snakes.h
@@ -0,0 +1,14 @@
+/*
+    Made by: 
+        - Diego Efraín Antonio Pérez 
+        - Armando Arredondo Valle
+*/
+
+#pragma once
+using namespace std;
+#include <string>
+#include "board.h"
+
+class Snakes: public Board{
+
+};
