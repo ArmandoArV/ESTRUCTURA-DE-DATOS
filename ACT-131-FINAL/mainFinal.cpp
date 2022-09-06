16ac31f1402b980a671a65ef84632fbbc7f99bd5
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-09-05 23:44:02 -0500

    Co-authored-by: TonyNogueron <TonyNogueron@users.noreply.github.com>

--- ACT-131-FINAL/mainFinal.cpp
+++ ACT-131-FINAL/mainFinal.cpp
@@ -7,8 +7,33 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 using namespace std;
+
+
+string readData(){
+    string data;
+    ifstream file;
+    file.open("DATA.dat");
+    if(file.is_open()){
+        getline(file, data);
+        // ignore the numbers and only take the letters
+        data = data.substr(4);
+        
+        file.close();
+    }
+    return data;
+}
+
+int validacion(int input,int inferior,int superior){
+    while (!(cin >> input) || input>superior || input<inferior) {
+        cout << "Input inválido. Por favor ingrese una de las opciones." << endl;
+        cin.clear(); //Limpia el input anterior
+        cin.ignore(123, '\n'); //Ignora caracteres anteriores
+    }
+    return input;
+} // Complexity: O(1)
 
 // Brute force string matching algorithm
 
@@ -33,7 +58,7 @@ void search_all(string const& text, string const& pattern) {
     }
     cout << "Total comparisons: " << total_comparisons << "\n";
     cout << "Total chars compared: " << total_comparisons * pattern_size << "\n";
-}
+} // Complexity: O(N*M) | N = text.size() | M = pattern.size()
 
 void compare_demo(string const& text, string const& pattern)
 {
@@ -65,10 +90,10 @@ void compare_demo(string const& text, string const& pattern)
     }
     cout << "Total comparisions: " << total_comparisions << "\n";                  // print the total comparisions
     cout << "Total chars compared: " << total_comparisions * pattern_size << "\n"; // print the total chars compared
-}
+} // Complexity: O(N*M) | N = text.size() | M = pattern.size()
 
 
-void iter_demo_bienhecho(string const& text, string const& pattern) { // iter_demo_bienhecho
+void iter_demo(string const& text, string const& pattern) { // iter_demo_bienhecho
     int char_compared = 0; // chars compared
     unsigned long const pattern_size(pattern.size()); // pattern size
     unsigned long const endpos(text.size() - pattern_size + 1); // end position
@@ -92,7 +117,7 @@ void iter_demo_bienhecho(string const& text, string const& pattern) { // iter_de
     }
     cout << "Total comparisons: " << endpos << "\n"; // print the total comparisons
     cout << "Total chars compared: " << char_compared << "\n"; // print the total chars compared
-}
+} // Complexity: O(N*M) | N = text.size() | M = pattern.size()
 
 struct PrefixResult {
     int prefix_length;
@@ -100,39 +125,39 @@ struct PrefixResult {
     int chars_compared;
 };
 
-PrefixResult find_prefix_suffix(string const& pattern){
-    int prefix_length = 0;
+PrefixResult find_prefix_suffix(string const& pattern){ //
+    int prefix_length = 0; //
     vector<int> first;
     int chars_compared = 0;
     PrefixResult answer;
-    for(int i=1; i < pattern.size(); ++i){
-        if(pattern[i] == pattern[0]){
-            first.push_back(i);
-            chars_compared++;
+    for(int i=1; i < pattern.size(); ++i){ // for every char inside the pattern
+        if(pattern[i] == pattern[0]){ // if the char that is being compared is equal to the first char
+            first.push_back(i); // push the index of the char to the vector
+            chars_compared++; // add 1 to the chars compared
         }
     }
-    if (first.empty()) {
-        answer.prefix_length = 0;
-        answer.chars_compared = chars_compared;
-        answer.positions = first;
-        return answer;
+    if (first.empty()) { // if the vector is empty
+        answer.prefix_length = 0; // set the prefix length to 0
+        answer.chars_compared = chars_compared; // set the chars compared to the chars compared
+        answer.positions = first; // set the positions to the vector
+        return answer; // return the answer
     }
-    for (int i = 0; i < first.size(); ++i){
-        int k = 1;
-        while(pattern[k] == pattern[first[i]+k]){
-            k++;
-            chars_compared++;
+    for (int i = 0; i < first.size(); ++i){ // for every index in the vector
+        int k = 1; // set k to 1
+        while(pattern[k] == pattern[first[i]+k]){ // while the char that is being compared is equal to the char in the pattern
+            k++; // add 1 to k
+            chars_compared++; // add 1 to the chars compared
         }
-        if(prefix_length < k){
-            prefix_length = k;
+        if(prefix_length < k){ // if the prefix length is less than k
+            prefix_length = k; // set the prefix length to k
         }
     }
 
-    answer.prefix_length = prefix_length;
-    answer.positions = first;
-    answer.chars_compared = chars_compared;
+    answer.prefix_length = prefix_length; // set the prefix length to the prefix length
+    answer.positions = first; // set the positions to the vector
+    answer.chars_compared = chars_compared; // set the chars compared to the chars compared
     return answer;
-}
+} // Complexity: O(M) |  M = pattern.size()
 
 struct CompareResult {
     bool isMatch;
@@ -143,26 +168,26 @@ struct CompareResult {
 CompareResult compare(int i, const string &pattern, unsigned long pattern_size, const string &text, vector<int> positions) {
     int chars_compared = 0;
     bool match = true;
-    int j = 0;
+    int j = 0; // j is the index of the pattern
     for(; j< pattern_size; ++j){
         if(text[j+i] == pattern[j]){ // if the char is equal to the pattern
-            chars_compared++;
-        } else {
-            chars_compared++;
-            match = false;
+            chars_compared++; // add 1 to the chars compared
+        } else { // if the char is not equal to the pattern
+            chars_compared++; // add 1
+            match = false; // set the match to false
             break;
         }
     }
-    CompareResult compare_result;
-    compare_result.isMatch = match;
-    compare_result.chars_compared = chars_compared;
-    if (positions.empty() || j < positions[0]){
-        compare_result.idx = i+j+1;
+    CompareResult compare_result; // create a compare result
+    compare_result.isMatch = match; // set the isMatch to the match
+    compare_result.chars_compared = chars_compared; // set the chars compared to the chars compared
+    if (positions.empty() || j < positions[0]){ // if the positions vector is empty or j is less than the first index in the vector
+        compare_result.idx = i+j+1; // set the index to i+j+1
     } else {
-        compare_result.idx = i+positions[0];
+        compare_result.idx = i+positions[0]; // set the index to i+the first index in the vector
     }
-    return compare_result;
-}
+    return compare_result; // return the compare result
+} // Complexity: O(M) | M = pattern.size()
 
 void smarter_search(string const& text, string const& pattern) { // smarter search
     int char_compared = 0; // chars compared
@@ -173,87 +198,115 @@ void smarter_search(string const& text, string const& pattern) { // smarter sear
     char_compared += prefix_result.chars_compared;
     for (int i=0; i < endpos;){ // for each index in the text
 
-        CompareResult result = compare(i, pattern, pattern_size, text, prefix_result.positions);
+        CompareResult result = compare(i, pattern, pattern_size, text, prefix_result.positions); 
 
-        if (result.isMatch) {
-            cout << i << " |" << text.substr(i,pattern_size) << "|  " << "1" << "  <---- match!" << "\n";
-        } else {
+        if (result.isMatch) { // if the match is true
+            cout << i << " |" << text.substr(i,pattern_size) << "|  " << "1" << "  <---- match!" << "\n"; // print the match and the index
+        } else { // if the match is false
             cout << i << " |" << text.substr(i,pattern_size) << "|  " << "0 " << "\n";
         }
-        char_compared += result.chars_compared;
-        i = result.idx;
-        comparisons++;
+        char_compared += result.chars_compared; // add the chars compared to the total chars compared
+        i = result.idx; // set i to the index
+        comparisons++; // add 1 to the comparisons
     }
     cout << "Total comparisons: " << comparisons << "\n"; // print the total comparisons
     cout << "Total chars compared: " << char_compared << "\n"; // print the total chars compared
-}
+} // Complexity: O(N*M) | N = text.size() | M = pattern.size() | It is better than brute force algorithms as it skips some backtracking
+
+
+void MPentry(string const& pattern, int pattern_size, int* MPtable, int& chars_compared){
+    int l = 0;
+    MPtable[0] = 0; // set the first index to 0
+    int i = 1;
+    while (i < pattern_size) { // while i is less than the pattern size
+        if (pattern[i] == pattern[l]) { // if the char is equal to the pattern
+            chars_compared++;
+            l++; // add 1 to l
+            MPtable[i] = l; // set the index to l 
+            i++; // add 1 to i
+        }
+        else //if the char isn't equal to the pattern 
+        {
+            chars_compared++; // add 1 to the chars compared
+            if (l != 0) { // if l is not equal to 0
+                chars_compared++; // add 1 to the chars compared
+                l = MPtable[l - 1]; // set l to the index of the table
+            }
+            else // if l is equal to 0
+            {
+                chars_compared++; // add 1 to the chars compared
+                MPtable[i] = 0; // set the index to 0
+                i++;
+            }
+        }
+    }
+} // Complexity: O(M) | M = pattern.size()
 
 void knuth_morris_pratt(string const& text, string const& pattern)
 {
     int const pattern_size(pattern.size());
     int const endpos(text.size() - pattern_size + 1);
+    int chars_compared = 0;
+
+    int MPTable[pattern_size]; // create a table
+    MPentry(pattern, pattern_size, MPTable, chars_compared); // call the MPentry function
     int i = 0;
     int j = 0;
-    int k = 0;
-    int MPTable[pattern_size];
-    MPTable[0] = 0;
-    for (int i = 1; i < pattern_size; i++)
+    while (i < endpos) // while i is less than the end position
     {
-        if (pattern[i] == pattern[j])
+        if (text[i] == pattern[j]) // if the char is equal to the pattern
         {
-            j++;
-            MPTable[i] = j;
+            chars_compared++; // add 1 to the chars compared
+            i++; // add 1 to i
+            j++; // add 1 to j
         }
-        else
-        {
-            if (j != 0)
-            {
-                j = MPTable[j - 1];
-                i--;
-            }
-            else
-            {
-                MPTable[i] = 0;
-            }
-        }
-    }
-    
-    for (int i = 0; i < pattern_size; i++)
-    {
-        cout << MPTable[i] << " ";
-    }
-    cout << "\n";
-    while (i < endpos)
-    {
-        if (text[i] == pattern[j])
-        {
-            i++;
-            j++;
-        }
-        if (j == pattern_size)
+        if (j == pattern_size) // if j is equal to the pattern size
         {
             cout << "Match at: " << i - j << "\n";
-            j = MPTable[j - 1];
+            j = MPTable[j - 1]; // set j to the index of the table
+            chars_compared++; // add 1 to the chars compared
         }
-        else if (i < endpos && text[i] != pattern[j])
+        else if (i < endpos && text[i] != pattern[j]) // if i is less than the end position and the char is not equal to the pattern
         {
-            if (j != 0)
+            chars_compared++; // add 1 to the chars compared
+            if (j != 0) // if j is not equal to 0
             {
-                j = MPTable[j - 1];
+                j = MPTable[j - 1]; // set j to the index of the table
+                chars_compared++; // add 1 to the chars compared
             }
-            else
+            else // if j is equal to 0
             {
-                i++;
+                i++; // add 1 to i
+                chars_compared++; // add 1 to the chars compared
             }
         }
     }
-}
+    cout << "Total comparisons: " << chars_compared << "\n"; // print the total chars compared
+} // Complexity: O(N+M) | N = text.size() | M = pattern.size()
+
+
+void mp_demo (string const& pattern) {
+    int pattern_size = pattern.size();
+    int MPTable[pattern_size];
+    int chars_compared = 0;
+    MPentry(pattern, pattern_size, MPTable, chars_compared);
+    cout << "|-| 0 | " << "-1" << " |" << endl;
+    for (int i = 1; i < pattern.size(); ++i) {
+        cout << "| " << pattern.substr(0,i) << " | " << i << " | " << MPTable[i-1] << " |" << endl;
+    }
+    cout << "| " << pattern << " | " << pattern.size() << " | " << "0" << " |" << endl;
+} // Complexity: O(2M) | M = pattern.size()
 
 int main() {
     string the_text = "panamanian banana fanatics can manage anacondas";
     string the_pattern = "ana";
+    string sequence1 = "CCTAAGG";
+    string sequence2 = "GCGGCCGC";
+    string sequence3 = "TCGA";
+    string original_sequence = "";
     int option = 0;
-    // Menu
+
+    
     do
     {
         cout << "----------------------------------------\n";
@@ -264,34 +317,39 @@ int main() {
         cout << "[3] Iter demo\n";
         cout << "[4] Smarter search\n";
         cout << "[5] Morris Pratt\n";
+        cout << "[6] Knuth-Morris_Pratt\n";
+        cout << "[7] KMP for Genetic code\n";
         cout << "[0] Exit\n";
         cout << "----------------------------------------\n";
         cout << "Option: ";
-        cin >> option;
+        option = validacion(option, 0,7);
+
         switch (option)
         {
-        case 1:
-            //search_all(the_text,the_pattern);
-            //search_all("aabcd","abcd");
-            search_all("aaaab","aaab");
-            break;
-        case 2:
-            //compare_demo(the_text,the_pattern);
-            compare_demo("aaaab","aaab");
-            break;
-        case 3:
-            iter_demo_bienhecho(the_text,the_pattern);
-            break;
-        case 4:
-            smarter_search(the_text,the_pattern);
-            break;
-        case 5:
-            //knuth_morris_pratt(the_text,the_pattern);
-            knuth_morris_pratt("abcabjofhabcababcabdabp","abcabdabp");
-            break;
-        default:
-            cout << "Invalid option!";
-            break;
+            case 1:
+                search_all(the_text,the_pattern);
+                break;
+            case 2:
+                compare_demo(the_text,the_pattern);
+                break;
+            case 3:
+                iter_demo(the_text,the_pattern);
+                break;
+            case 4:
+                smarter_search(the_text,the_pattern);
+                break;
+            case 5:
+                mp_demo(the_pattern);
+                break;
+            case 6:
+                knuth_morris_pratt(the_text,the_pattern);
+                break;
+            case 7:
+                knuth_morris_pratt(readData(),sequence1);
+                knuth_morris_pratt(readData(),sequence2);
+                knuth_morris_pratt(readData(),sequence3);
+                break;
         }
     } while (option != 0);
+    cout << "Ending..." << endl;
 }
