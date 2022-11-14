94115a14a5c1ce28860eca2551292a0b98cdc481
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-11-14 09:34:38 -0600

    Added graph class.

--- Integradora-Grafos/main.cpp
+++ Integradora-Grafos/main.cpp
@@ -2,6 +2,10 @@
 #include "Graph.h"
 using namespace std;
 
+bool sortbysec(const pair<string,int> &a, const pair<string,int> &b) { // Time Complexity O(1)
+    return (a.second > b.second);
+}
+
 int main() {
     cout << "Hello, World!\n";
     return 0;
